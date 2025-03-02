4d401118d0ac08374e83fb8326b2a52e094569da
codexllamma <codexllamma@gmail.com>
2025-03-02 10:18:35 +0530

    hi

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-document.getElementById('new-link').addEventListener('click', () => {
-  window.location.href = "newpage.html"
-})
+
+
+document.getElementById('new-link').addEventListener("click", (event) => {
+  //event.preventDefault(); // Prevents immediate navigation
+
+  let btn = document.getElementById('new-link'); // Get the element
+  btn.classList.add("move-left"); // Add animation class
+
+  setTimeout(() => {
+      btn.style.display = "none";
+      ul.style.display = "none";
+      window.location.href = "newpage.html"; // Hides element after animation
+  }, 500);
+  
+  
+  let ul = document.getElementById("nav-bar"); // Get UL element
+  ul.classList.add("fade-out"); // Add fade-out class
+
+  setTimeout(() => {
+       ul.style.display = "none"; // Hide UL after animation
+  }, 500);// Matches animation duration
+
+  
+});
